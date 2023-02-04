eeae0eb6f576a13e68bea5ecaf4de59f78d7c323
LuisSS20 <luissoriano@correo.ugr.es>
2023-02-04 19:52:02 +0000

    Add new windows installer

--- windows/flutter/ephemeral/generated_config.cmake
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -1,6 +1,6 @@
 # Generated code do not commit.
-file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
-file(TO_CMAKE_PATH "C:\\Users\\bobge\\Documents\\TFG\\tree_timer_app" PROJECT_DIR)
+file(TO_CMAKE_PATH "C:\\hostedtoolcache\\windows\\flutter\\stable-3.7.1-x64" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\a\\tree_timer_app\\tree_timer_app" PROJECT_DIR)
 
 set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
 set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
@@ -10,14 +10,14 @@ set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
 
 # Environment variables to pass to tool_backend.sh
 list(APPEND FLUTTER_TOOL_ENVIRONMENT
-  "FLUTTER_ROOT=C:\\flutter"
-  "PROJECT_DIR=C:\\Users\\bobge\\Documents\\TFG\\tree_timer_app"
-  "FLUTTER_ROOT=C:\\flutter"
-  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\bobge\\Documents\\TFG\\tree_timer_app\\windows\\flutter\\ephemeral"
-  "PROJECT_DIR=C:\\Users\\bobge\\Documents\\TFG\\tree_timer_app"
+  "FLUTTER_ROOT=C:\\hostedtoolcache\\windows\\flutter\\stable-3.7.1-x64"
+  "PROJECT_DIR=D:\\a\\tree_timer_app\\tree_timer_app"
+  "FLUTTER_ROOT=C:\\hostedtoolcache\\windows\\flutter\\stable-3.7.1-x64"
+  "FLUTTER_EPHEMERAL_DIR=D:\\a\\tree_timer_app\\tree_timer_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\a\\tree_timer_app\\tree_timer_app"
   "FLUTTER_TARGET=lib\\main.dart"
   "DART_OBFUSCATION=false"
   "TRACK_WIDGET_CREATION=true"
   "TREE_SHAKE_ICONS=true"
-  "PACKAGE_CONFIG=C:\\Users\\bobge\\Documents\\TFG\\tree_timer_app\\.dart_tool\\package_config.json"
+  "PACKAGE_CONFIG=D:\\a\\tree_timer_app\\tree_timer_app\\.dart_tool\\package_config.json"
 )
