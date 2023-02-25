c367f782c408f74d585a50eb8d6f2f8c072afbe0
LuisSS20 <bobgenge@gmail.com>
2023-02-25 14:09:52 +0100

    Add treespecies router in index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cookieParser from "cookie-parser";
 import dotenv from 'dotenv';
 import authUserRouter from './routes/auth_users.js';
+import treeSpeciesRouter from './routes/tree_species.js';
 import mongoose from 'mongoose';
 
 // Cargamos valores de .env en process
@@ -19,7 +20,8 @@ expressApp.use(express.json());
 expressApp.use(express.text());
 
 // expressApp.use("/auth-token", authTokenRouter);
-expressApp.use("/accounts", authUserRouter)
+expressApp.use("/accounts", authUserRouter);
+expressApp.use("/treespecies", treeSpeciesRouter);
 
 // Esta funcion e spara arrancar la app ya que connect es async
 const bootstrap = async () =>{
