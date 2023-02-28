29d7fc7a933186928ea2f997eda81b19846664ad
LuisSS20 <bobgenge@gmail.com>
2023-02-28 17:09:36 +0100

    Modify tree species screen to add list

--- lib/screens/tree_species.dart
+++ lib/screens/tree_species.dart
@@ -20,57 +20,59 @@ class _TreeSpecies extends State<TreeSpecies>{
       appBar: AppBar(
         title: Text('Especies disponibles'),
       ),
-      body: Container(
-        margin: const EdgeInsets.all(70),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              FutureBuilder(
-                future: treeSpecieService.obtenerEspecies(),
-                builder: (context, snapshot) {
-                  // If we have data from tree species
-                  if(snapshot.hasData)
-                  {
-                    return Column(
-                      children: [
-                        Container(
-                          // We must to set height and width in order to prevent errors
-                          // with listView dimensions
-                          width: 400,
-                          height: 400,
-                          child: ListView.builder(
-                            itemCount: snapshot.data.length,
-                            itemBuilder: (context, index) {
-                              return ListTile(
-                                leading: Icon(Icons.eco, color: Colors.green,),
-                                title: Text(snapshot.data[index]["name"] + " (${snapshot.data[index]["description"]})"),
-                                // subtitle: Text(snapshot.data[index]["description"]),
-                                onTap: () {
-                                },
-                              );
-                            },
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.all(70),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FutureBuilder(
+                  future: treeSpecieService.obtenerEspecies(),
+                  builder: (context, snapshot) {
+                    // If we have data from tree species
+                    if(snapshot.hasData)
+                    {
+                      return Column(
+                        children: [
+                          Container(
+                            // We must to set height and width in order to prevent errors
+                            // with listView dimensions
+                            width: 400,
+                            height: 400,
+                            child: ListView.builder(
+                              itemCount: snapshot.data.length,
+                              itemBuilder: (context, index) {
+                                return ListTile(
+                                  leading: Icon(Icons.eco, color: Colors.green,),
+                                  title: Text(snapshot.data[index]["name"] + " (${snapshot.data[index]["description"]})"),
+                                  // subtitle: Text(snapshot.data[index]["description"]),
+                                  onTap: () {
+                                  },
+                                );
+                              },
+                            ),
                           ),
-                        ),
-                        ElevatedButton(
-                          onPressed: (){
-                            Navigator.pop(context);
-                          },
-                          child: Text("Salir")
-                        )
-                      ]
-                    );
+                          ElevatedButton(
+                            onPressed: (){
+                              Navigator.pop(context);
+                            },
+                            child: Text("Salir")
+                          )
+                        ]
+                      );
+                    }
+                    else if(snapshot.hasError){
+                      showSnackBar(context, snapshot.error.toString());
+                    }
+                    return CircularProgressIndicator();
                   }
-                  else if(snapshot.hasError){
-                    showSnackBar(context, snapshot.error.toString());
-                  }
-                  return CircularProgressIndicator();
-                }
-              ),
-            ],
+                ),
+              ],
+            ),
           ),
         ),
-      )
+      ) 
     );
   }
 }
