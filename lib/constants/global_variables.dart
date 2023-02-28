fdcc2e11d9689cc3dcb185a4bb548dd9c4e311b8
LuisSS20 <bobgenge@gmail.com>
2023-02-28 22:49:31 +0100

    Add new branch constant

--- lib/constants/global_variables.dart
+++ lib/constants/global_variables.dart
@@ -3,5 +3,8 @@ import 'package:flutter/material.dart';
 // Localhost
 String url = 'http://10.0.2.2:6000';
 
-//NodeJS Server
+//NodeJS Server MAIN BRANCH
 // String url = 'https://nodejs-server-vtym.onrender.com';
+
+//NodeJS Server MAIN BRANCH
+// String url = 'https://node-js-server-project-branch.onrender.com';
