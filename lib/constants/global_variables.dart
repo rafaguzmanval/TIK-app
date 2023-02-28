b27c517ddd56b78b1a07de59dfc4938317ad71e1
LuisSS20 <bobgenge@gmail.com>
2023-02-28 14:03:50 +0100

    Modify global constants

--- lib/constants/global_variables.dart
+++ lib/constants/global_variables.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 // Localhost
-String url = 'http://10.0.2.2:5500';
+String url = 'http://10.0.2.2:6000';
 
 //NodeJS Server
 // String url = 'https://nodejs-server-vtym.onrender.com';
