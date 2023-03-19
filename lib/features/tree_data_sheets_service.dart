4b88c01c1dc717720c12572f070d6676fea669c6
LuisSS20 <bobgenge@gmail.com>
2023-03-19 02:09:35 +0100

    Add update, find and delete services

--- lib/features/tree_data_sheets_service.dart
+++ lib/features/tree_data_sheets_service.dart
@@ -46,6 +46,44 @@ class TreeDataSheetService{
     }
   }
 
+  
+  void updateTreeDataSheet({
+    required BuildContext context,
+    required String id,
+    required String project_id,
+    required TreeSpecie treeSpecie,
+    required String treeId,
+    String? description
+  })
+  async{
+
+    try{
+      TreeDataSheet treeDataSheet = TreeDataSheet(
+        id: id,
+        project_id: project_id,
+        specific_tree_id: treeId,
+        tree_specie_id: treeSpecie.id,
+        description: description,
+      );
+
+      http.Response res = await http.put(
+        Uri.parse('$url/treedatasheets/update/${treeDataSheet.id}'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: treeDataSheet.toJson(),
+      );
+
+      httpErrorHandler(res: res, context: context,
+        onSuccess: (){
+          showSnackBar(context, "¡Ficha de datos actualizada correctamente!");
+        }
+      );
+    } catch(err){
+      showSnackBar(context, err.toString());
+    }
+  }
+
 // Get user data, for init state
   Future<dynamic> getProjectTreeDataSheets(String projectid) async {
     final response = await http.get(
@@ -60,5 +98,38 @@ class TreeDataSheetService{
       throw Exception('Error al obtener las fichas de datos del proyecto');
     }
   }
-  
+
+  Future<dynamic> findTreeDataSheet(String _id) async {
+    final response = await http.get(
+        Uri.parse('$url/treedatasheets/${_id}'),
+      );
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> listJson = json.decode(response.body);
+
+      return listJson;
+    } else {
+      throw Exception('Error al obtener la ficha de datos');
+    }
+  }
+
+  void deleteTreeDataSheet({required BuildContext context, required String id}) async {
+    try
+    {
+      final response = await http.delete(
+        Uri.parse('$url/treedatasheets/delete/${id}'),
+      );
+
+      httpErrorHandler(res: response, context: context,
+        onSuccess: (){
+          showSnackBar(context, "¡Ficha de datos borrada correctamente!");
+        }
+      );
+      
+    }
+    catch(err){
+      showSnackBar(context, err.toString());
+    } 
+  }
+
 }
