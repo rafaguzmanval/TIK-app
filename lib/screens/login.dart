3627aec86fd22371202b2503c880258179e56049
LuisSS20 <bobgenge@gmail.com>
2023-03-21 15:54:50 +0100

    Add rive animation

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -4,6 +4,7 @@ import 'package:tree_timer_app/common/widgets/custom_button.dart';
 import 'package:tree_timer_app/common/widgets/custom_textformfield.dart';
 import 'package:tree_timer_app/features/auth_service.dart';
 import 'package:tree_timer_app/providers/user_provider.dart';
+import 'package:rive/rive.dart';
 
 
 class Login extends StatefulWidget {
@@ -38,41 +39,53 @@ class _LoginState extends State<Login> {
       ),
       body: Container(
         margin: EdgeInsets.all(50),
-        child: Center(
-          child: Form(
-            key: _loginFormKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+        child: Column(
+          children: [
+            Stack(
               children: [
-                CustomTextField(controller: _emailController, hintText: "Email"),
-                SizedBox(height: 15),
-                CustomTextField(controller: _passwordController, hintText: "Contraseña", isPassword: true,),
-                SizedBox(height: 35),
-                Container(
-                  width: 200,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: CustomButton(
-                    text: "Login",
-                    textStyle: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-                    onTap: (){
-                      if(_loginFormKey.currentState!.validate()) {
-                        loginUser();
-                      } 
-                    },
-                  ),
-                ),
-                SizedBox(height: 35),
-                GestureDetector(
-                  onTap:(){
-                    Navigator.pushNamed(context, '/register');
-                  },
-                  child: const Text("¿Nuevo usuario? Cree una cuenta")
-                ),
+                SizedBox(
+                  width: 250,
+                  height: 250,
+                  child: RiveAnimation.asset("assets/rive/tree.riv", fit: BoxFit.cover,))
+                // RiveAnimation.asset("assets/rive/tree.riv", fit: BoxFit.cover,)
+                // RiveAnimation.asset("tree.riv")
               ],
             ),
-          ),
+            Form(
+              key: _loginFormKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CustomTextField(controller: _emailController, hintText: "Email"),
+                  SizedBox(height: 15),
+                  CustomTextField(controller: _passwordController, hintText: "Contraseña", isPassword: true,),
+                  SizedBox(height: 35),
+                  Container(
+                    width: 200,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: CustomButton(
+                      text: "Login",
+                      textStyle: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                      onTap: (){
+                        if(_loginFormKey.currentState!.validate()) {
+                          loginUser();
+                        } 
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 35),
+                  GestureDetector(
+                    onTap:(){
+                      Navigator.pushNamed(context, '/register');
+                    },
+                    child: const Text("¿Nuevo usuario? Cree una cuenta")
+                  ),
+                ],
+              ),
+            ),
+          ] 
         ),
       ),
     );
