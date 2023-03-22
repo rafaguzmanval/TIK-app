cd8e85b22c5b623cb4e1f09edfec7d611eaa1732
LuisSS20 <bobgenge@gmail.com>
2023-03-22 22:33:14 +0100

    Change global constants

--- lib/constants/global_variables.dart
+++ lib/constants/global_variables.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 // String url = 'http://10.0.2.2:6000';
 
 //NodeJS Server MAIN BRANCH
-String url = 'https://nodejs-server-vtym.onrender.com';
+// String url = 'https://nodejs-server-vtym.onrender.com';
 
 //NodeJS Server MAIN BRANCH
-// String url = 'https://node-js-server-project-branch.onrender.com';
+String url = 'https://node-js-server-project-branch.onrender.com';
