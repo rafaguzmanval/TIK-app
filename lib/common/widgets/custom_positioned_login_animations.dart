7c93c6cba8bbbb647b8d6d1930c76998793fdfc1
LuisSS20 <bobgenge@gmail.com>
2023-04-06 22:07:29 +0200

    Add new parameters

--- lib/common/widgets/custom_positioned_login_animations.dart
+++ lib/common/widgets/custom_positioned_login_animations.dart
@@ -5,22 +5,39 @@ import 'package:flutter/material.dart';
 class CustomPositionedLoginAnimation extends StatelessWidget
 {
   final Widget child;
+  final double  height;
+  final double  width;
+  final double left;
+  final double top;
+  final double right;
+  final double bottom;
 
-  const CustomPositionedLoginAnimation({super.key, required this.child});
+  const CustomPositionedLoginAnimation({super.key, required this.child, this.height = 125, this.width = 125,
+                                        this.left = 0, this.right = 0, this.top = 0, this.bottom = 0});
 
   @override
   Widget build(BuildContext context) {
     return Positioned.fill(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 125,
-                  width: 125,
-                  child: this.child
-                  ),
-              ],
-            )
-          );
+      left: left,
+      right: right,
+      top: top,
+      bottom: bottom,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: SizedBox(
+                height: height,
+                width: width,
+                child: this.child
+                ),
+            ),
+          ),
+        ],
+      )
+    );
   }
 
 }
